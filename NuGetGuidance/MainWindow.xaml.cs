03bef932667ba730733e3c0e8fe7cf0e6e32be4f
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-04-04 12:21:54 +0200

    Updated LICENSE

--- NuGetGuidance/MainWindow.xaml.cs
+++ NuGetGuidance/MainWindow.xaml.cs
@@ -1,24 +1,4 @@
-﻿#region License
-
-// 
-//  Copyright 2012 Steven Thuriot
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// 
-
-#endregion
-
-using System;
+﻿using System;
 using System.ComponentModel;
 using System.Globalization;
 using System.Runtime.InteropServices;
@@ -32,7 +12,6 @@ using System.Windows.Threading;
 using Microsoft.Win32;
 using NuGetGuidance.Domain;
 using NuGetGuidance.Handlers;
-using Application = System.Windows.Application;
 using RESX = NuGetGuidance.Properties.Resources;
 
 namespace NuGetGuidance
